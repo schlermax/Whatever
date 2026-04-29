065440a84339a676e60a5a69843e052cb9ca8dd7
Maxwell Schleicher <schlermax5@gmail.com>
2026-04-28 20:38:17 -0400

    ci/cd pipeline

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: isProd ? "export" : undefined,
+  basePath: isProd ? "/Next.js-Lesson-Plans" : "",
+  assetPrefix: isProd ? "/Next.js-Lesson-Plans/" : "",
+  images: {
+    unoptimized: isProd,
+  },
 };
 
 export default nextConfig;
